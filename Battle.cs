09fdaa99b5ca1ccd8b1ad00805177c46ef24edd7
Alexander <temnoved@yandex.ru>
2016-09-28 20:04:11 +0300

    Add files via upload
    Start

---
+++ Battle.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7Tanks
+{
+    enum Teamwinner { T34 = 1, Panters, Tie}//перечисление возможных исходов сражения
+    class Battle
+    {
+        const int iTeamQuant = 5;//максимальное количество юнитов в команде
+        protected Tanks [] tT34 = null;//массив для первой команды
+        protected Tanks [] tPant = null;//массив для второй команды
+
+        public Battle(Tanks[] t1 = null, Tanks[] t2 = null)//конструктор
+        {
+            this.tT34 = t1;
+            this.tPant = t2;
+        }
+
+        public Teamwinner Winner()//определение побидителя
+        {
+            if(this.tPant.Length == 0 || this.tT34.Length == 0)//проверка на пустые массивы
+            {
+                throw new Exception("Arrey is empty(class Battle.Winner)");
+            }
+            int iCountT1 = 0, iCountT2 = 0;
+            for(int i = 0; i < iTeamQuant; i++)
+            {
+                //перегруженная операция * возврощает 1 если победил перывый юнит
+                //возвращает -1 если победил второй юнит
+                //возвращает 0 если ничья
+                if((this.tT34[i]*this.tPant[i]) > 0 )//
+                {
+                    iCountT1++;
+                }
+                if ((this.tT34[i] * this.tPant[i]) <0)
+                {
+                    iCountT2++;
+                }
+            }
+            //по окончании цикла возвращаем один из возможных результатов в зависимости от состояния счетчиков
+            if (iCountT1 > iCountT2)
+            {
+                return Teamwinner.T34;
+            }
+            if (iCountT1 < iCountT2)
+            {
+                return Teamwinner.Panters;
+            }
+            return Teamwinner.Tie;  
+        }
+
+        public void ShowTeams()//метод показа команд
+        {
+            if (this.tPant.Length == 0 || this.tT34.Length == 0)//проверка на пустые массивы
+            {
+                throw new Exception("Arrey is empty(class Battle.ShowTeams)");
+            }
+            Console.WriteLine("Team T-34:" + new String(' ', 30) + "Team Panter:");
+            for(int i = 0; i < iTeamQuant; i++)
+            {
+                this.tT34[i].ShowTank();
+                Console.Write("\t");
+                this.tPant[i].ShowTank();
+                Console.WriteLine();
+            }
+            Console.WriteLine(new String('*', 75));
+        }
+
+        public void ShowWinner(Teamwinner tWin)//метод показа результата
+        {
+            if ((int)tWin < 1 || (int)tWin > 3)//проверка попадания результата в допустимый диапазон
+            {
+                throw new Exception("Arrey is empty(class Battle.ShowTeams)");
+            }
+
+            if (tWin == Teamwinner.T34)
+            {
+                Console.WriteLine("Battle result: Team T-34 - Winner");
+            }
+            if (tWin == Teamwinner.Panters)
+            {
+                Console.WriteLine("Battle result: Team Panters - Winner");
+            }
+            if (tWin == Teamwinner.Tie)
+            {
+                Console.WriteLine("Battle result: Tie");
+            }
+        }
+    }
+}
---
+++ Tanks.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7Tanks
+{
+    class Tanks
+    {
+        static Random rnd = new Random();//дял случайного заполнения
+        protected String strName = "";//имя
+        protected byte bArmor = 0;//броня
+        protected byte bAmo = 0;//боезапас
+        protected byte bMobility = 0;//мобильность
+
+        public Tanks(String strNam = "")//конструктор с именем
+        {
+            this.strName = strNam;
+            this.bArmor = (byte)rnd.Next(0, 101);
+            this.bAmo = (byte)rnd.Next(0, 101);
+            this.bMobility = (byte)rnd.Next(0, 101);
+        }
+
+        public Tanks()//конструктор без параметров
+        {
+            char[] chTmp = new char[5];//для генерации имени
+            for (int i = 0; i < 5; i++)//созадём случайный набор из больших букв
+            {
+                chTmp[i] = (char)rnd.Next(65, 91);
+            }
+            this.strName = chTmp.ToString();
+            //случайное заполнение полей
+            this.bArmor = (byte)rnd.Next(0, 101);
+            this.bAmo = (byte)rnd.Next(0, 101);
+            this.bMobility = (byte)rnd.Next(0, 101);
+        }
+
+        static int ArmorCompare(Tanks tObj1, Tanks tObj2)//сравнение по броне
+        {
+            if(tObj1.bArmor > tObj2.bArmor)
+            {
+                return 1;
+            }
+            if(tObj1.bArmor < tObj2.bArmor)
+            {
+                return -1;
+            }
+            return 0;
+        }
+
+        static int AmoCompare(Tanks tObj1, Tanks tObj2)//сравнение по боезапасу
+        {
+            if (tObj1.bAmo > tObj2.bAmo)
+            {
+                return 1;
+            }
+            if (tObj1.bAmo < tObj2.bAmo)
+            {
+                return -1;
+            }
+            return 0;
+        }
+
+        static int MobilityCompare(Tanks tObj1, Tanks tObj2)//сравнение по мобильности
+        {
+            if (tObj1.bMobility > tObj2.bMobility)
+            {
+                return 1;
+            }
+            if (tObj1.bMobility < tObj2.bMobility)
+            {
+                return -1;
+            }
+            return 0;
+        }
+        public static short operator * (Tanks tObj1, Tanks tObj2)//определение победителя в сражении пары юнитов
+        {
+            short CountObj = 0;
+            CountObj += (short)ArmorCompare(tObj1, tObj2);
+            CountObj += (short)AmoCompare(tObj1, tObj2);
+            CountObj += (short)MobilityCompare(tObj1, tObj2);
+            //вернёт отрицательное значение если победил юнит 2
+            //вернёт положительное значение если победил юнит 1
+            //вернёт 0 если ничья
+            return CountObj;
+        }
+
+
+        public void ShowTank()//метод показа характеристик юнита в виде строки
+        {
+            if(this.strName == "")//если имени нет генерируем исключение
+            {
+                throw new Exception("The tank has empty name(class Tanks.ShowTank)");
+            }
+            Console.Write("Name:" + this.strName + " Arm:" + this.bArmor.ToString() + " Amo:" + this.bAmo + " Mobi:" + this.bMobility);
+        }
+    }
+}
